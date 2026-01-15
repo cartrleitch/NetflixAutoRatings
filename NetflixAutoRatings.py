27b4861d103233411ba11af09ad3bbdd7be1ab9c
Carter <122408763+cartrleitch@users.noreply.github.com>
2026-01-14 20:06:08 -0600

    Add files via upload

--- NetflixAutoRatings.py
+++ NetflixAutoRatings.py
@@ -1,8 +1,22 @@
-episode_title = input("Enter the episode title: ")
+episode_title = "I'm Luffy! The Man Who Will Become the Pirate King!"
+
+def get_rating_by_episode_id(episode_id):
+    ratings = open("title.ratings.tsv", "r", encoding="utf-8")
+    next(ratings)  # Skip header line
+
+    for rating_line in ratings:
+        split_rating_data = rating_line.split("\t")
+        rating_title_id = split_rating_data[0]
+        average_rating = split_rating_data[1]
+
+        if episode_id == rating_title_id:
+                rating_line = f"Episode ID {episode_id} has an average rating of {average_rating}."
+                return average_rating
+     
+    return "Rating not found."
 
 def get_rating_by_title(title):
     titles = open("title.basics.tsv", "r", encoding="utf-8")
-    ratings = open("title.ratings.tsv", "r", encoding="utf-8")
     target_title_id = ""
 
     for title_line in titles:
@@ -18,14 +32,24 @@ def get_rating_by_title(title):
     print("Retrieving average rating...")
     print (target_title_id)
 
-    for rating_line in ratings:
-        split_rating_data = rating_line.split("\t")
-        rating_title_id = split_rating_data[0]
-        average_rating = split_rating_data[1]
-
-        if target_title_id == rating_title_id:
-                rating_line = f"{primary_title} has an average rating of {average_rating}."
-                return average_rating
+    return get_rating_by_episode_id(target_title_id)
      
-print(get_rating_by_title(episode_title))
+print(f'Average rating for desired episode: {get_rating_by_title(episode_title)}')
+
+def get_rating_by_episode(target_parent_id, target_episode_num):
+    episodes = open("title.episode.tsv", "r", encoding="utf-8")
+    next(episodes) # Skip header line
+
+    for episode_line in episodes:
+        split_episode_data = episode_line.split("\t")
+        episode_num = split_episode_data[3].strip()
+        parent_id = split_episode_data[1].lower()
     
+        if target_parent_id.lower() == parent_id and target_episode_num == episode_num:
+            episode_id = split_episode_data[0]
+            print(f'Episode id: {episode_id} for episode number {episode_num} found. Parent id: {parent_id}')
+            return get_rating_by_episode_id(episode_id)
+            
+    return "Rating not found." 
+
+# print(f'Average rating for desired episode: {get_rating_by_episode("tt0388629", "723")}')  # One Piece 
