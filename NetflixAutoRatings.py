b79e95d9a05eaad35dd75f8540b4174dfcbd0453
Carter <122408763+cartrleitch@users.noreply.github.com>
2026-01-16 20:41:05 -0600

    Add files via upload

--- NetflixAutoRatings.py
+++ NetflixAutoRatings.py
@@ -1,7 +1,10 @@
 import sys
 import os
+import DatasetCache as cache
 
 episode_title = "I'm Luffy! The Man Who Will Become the Pirate King!"
+
+ratings_dict, episodes_dict, series_dict = cache.load_cache()
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -12,23 +15,8 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 def get_rating_by_episode_id(episode_id):
-    try:
-        ratings = open(resource_path("title.ratings.tsv"), "r", encoding="utf-8")
-    except:
-        return "Error."
-    next(ratings)  # Skip header line
-
-    for rating_line in ratings:
-        split_rating_data = rating_line.split("\t")
-        rating_title_id = split_rating_data[0]
-        average_rating = split_rating_data[1]
-
-        if episode_id == rating_title_id:
-                rating_line = f"Episode ID {episode_id} has an average rating of {average_rating}."
-                return average_rating
+    return ratings_dict.get(episode_id, "Rating not found.")
      
-    return "Rating not found."
-
 def get_rating_by_title(title):
     try:
         titles = open(resource_path("title.basics.tsv"), "r", encoding="utf-8")
@@ -55,24 +43,10 @@ def get_rating_by_title(title):
 #print(f'Average rating for desired episode: {get_rating_by_title(episode_title)}')
 
 def get_rating_by_episode(target_parent_id, target_episode_num):
-    try:
-        episodes = open(resource_path("title.episode.tsv"), "r", encoding="utf-8")
-    except:
-        return "Error."
-    next(episodes) # Skip header line
-
-    for episode_line in episodes:
-        split_episode_data = episode_line.split("\t")
-        episode_num = split_episode_data[3].strip()
-        parent_id = split_episode_data[1].lower()
-    
-        if target_parent_id.lower() == parent_id and target_episode_num == episode_num:
-            episode_id = split_episode_data[0]
-            print(f'Episode id: {episode_id} for episode number {episode_num} found. Parent id: {parent_id}')
-            return get_rating_by_episode_id(episode_id)
-            
-    return "Rating not found." 
-
+    episode_id = episodes_dict.get((target_parent_id.lower(), str(target_episode_num)), "Episode ID not found.")
+    if episode_id == "Episode ID not found.":
+        return "Episode ID not found."
+    return ratings_dict.get(episode_id, "Rating not found.")
 # print(f'Average rating for desired episode: {get_rating_by_episode("tt0388629", "601")}')  # One Piece 
 
 def get_series_id_by_title(series_title):
